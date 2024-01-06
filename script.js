3c2659f85de400f42b3099312539d0b6b78783d1
TheAlexGET <sucevanalex06@gmail.com>
2024-01-06 09:57:13 +0100

    stable dumb version

--- script.js
+++ script.js
@@ -1,51 +1,150 @@
+class Brick {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+// Entries
+const enterBricks = [
+  new Brick(500, 500),
+  new Brick(200, 200),
+  new Brick(300, 250),
+  new Brick(100, 100),
+  new Brick(70, 70),
+  new Brick(70, 70),
+  new Brick(50, 700),
+  new Brick(50, 600),
+  new Brick(750, 50),
+  new Brick(50, 50),
+  new Brick(50, 50),
+];
+
 //Logic
+class Canvas {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.bricks = [
+      { left: 0, top: 0, width: 1, height: this.height, color: "#6185f8" },
+      {
+        left: this.width - 1,
+        top: 0,
+        width: 1,
+        height: this.height,
+        color: "#6185f8",
+      },
+    ];
+    this.internalGaps = 0;
+  }
 
-// Need a liveserver for this
-const enterBricks = [{width: 100, height: 100,}, {width: 100, height: 100,}]
+  addBrick(brick, left, top, rotated = false) {
+    if (rotated) {
+      [brick.width, brick.height] = [brick.height, brick.width];
+    }
+    this.bricks.push({ ...brick, left, top, rotated });
+    this.updateInternalGaps();
+  }
 
-const exitParams = {
-  fullness: 1,
-  exitBricks: [],
-};
+  countFullness() {
+    const brickArea = this.bricks.reduce(
+      (area, brick) => (area += brick.width * brick.height),
+      0
+    );
+    const fullness = 1 - this.internalGaps / (this.internalGaps + brickArea);
+    return fullness;
+  }
 
-function searchPlace(bricks, ctx) {
-  const freePlace = {
-    left: 0,
-    top: ctx.canvas.height,
-  };
+  updateInternalGaps() {
+    const grid = Array.from({ length: this.height }, () =>
+      Array(this.width).fill(false)
+    );
 
-  let height = ctx.canvas.height;
-  for (let i = 0; i < bricks.length; i++) {
-    if (i === 0) {
-      exitParams.exitBricks.push({
-        top: height - bricks[i].height,
-        left: 0,
-        bottom: 0,
-        right: ctx.canvas.width - bricks[0].width,
-        initialOrder: 0,
-        color: "",
-      });
-    } else {
-      exitParams.exitBricks.push({
-        top: height - bricks[i].height - bricks[i-1].height - 40,
-        left: 80,
-        bottom: 0,
-        right: ctx.canvas.width - bricks[i - 1].width,
-        initialOrder: i,
-        color: "",
-      });
+    for (const brick of this.bricks) {
+      for (let i = brick.top; i < brick.top + brick.height; i++) {
+        for (let j = brick.left; j < brick.left + brick.width; j++) {
+          try {
+            grid[i][j] = true;
+          } catch (e) {
+            // nothing
+          }
+        }
+      }
+    }
+
+    this.internalGaps = 0;
+    grid.reverse();
+    for (let i = 1; i < this.height - 1; i++) {
+      for (let j = 1; j < this.width - 1; j++) {
+        if (!grid[i][j] && grid[i][j - 1] && grid[i - 1][j]) {
+          let startPos = j;
+          let endPos = j;
+          while (grid[i - 1][endPos] && !grid[i][endPos]) {
+            if (
+              (!grid[i - 1][endPos + 1] && grid[i][endPos + 1]) ||
+              (grid[i - 1][endPos + 1] && grid[i][endPos + 1])
+            ) {
+              this.internalGaps += endPos + 1 - startPos;
+              grid[i].fill(true, startPos, endPos + 1);
+            }
+            endPos += 1;
+          }
+        }
+      }
     }
   }
 }
 
-const countFullness = (enterBricks, exitBricks, ctx) => {
-  let fullness = 1;
-  let bricksArea =  0
-  for (let i = 0; i < exitBricks.length; i++) {
-    
+function isOverlap(canvas, brick, left, top, rotated = false) {
+  const checkBrick = rotated
+    ? { width: brick.height, height: brick.width }
+    : brick;
+  for (const placedBrick of canvas.bricks) {
+    if (
+      left < placedBrick.left + placedBrick.width &&
+      left + checkBrick.width > placedBrick.left &&
+      top < placedBrick.top + placedBrick.height &&
+      top + checkBrick.height > placedBrick.top
+    ) {
+      return true;
+    }
   }
-  return fullness;
-};
+  return false;
+}
+
+function findBestPlace(canvas, brick) {
+  for (let top = 0; top <= canvas.height - brick.height; top++) {
+    for (let left = 0; left <= canvas.width - brick.width; left++) {
+      if (!isOverlap(canvas, brick, left, top)) {
+        return { left, top, rotated: false };
+      } else if (
+        brick.width !== brick.height &&
+        !isOverlap(canvas, brick, left, top, true)
+      ) {
+        return { left, top, rotated: true };
+      }
+    }
+  }
+  return null;
+}
+
+function findBestBrickLayout(bricks, canvaWidth, canvaHeight) {
+  bricks.sort((a, b) => b.width * b.height - a.width * a.height);
+  bricks.forEach((brick, index) => {
+    brick.initialOrder = index;
+  });
+
+  const canvas = new Canvas(canvaWidth, canvaHeight);
+
+  for (const brick of bricks) {
+    const bestPlace = findBestPlace(canvas, brick);
+
+    if (bestPlace) {
+      canvas.addBrick(brick, bestPlace.left, bestPlace.top, bestPlace.rotated);
+    }
+  }
+  return canvas;
+}
 
 const getRandomColor = () => {
   const letters = "0123456789ABCDEF";
@@ -56,53 +155,36 @@ const getRandomColor = () => {
   return color;
 };
 
-function drawBricks(exitBricks, enterBricks, ctx) {
-  for (let i = 0; i < exitBricks.length; i++) {
-    for (let j = 1; j < exitBricks.length; j++) {
-      if (
-        enterBricks[i].width == enterBricks[j].width &&
-        enterBricks[i].height == enterBricks[j].height &&
-        !exitBricks.color
-      ) {
-        exitBricks[i].color
-          ? (exitBricks[j].color = exitBricks[i].color)
-          : ((ctx.fillStyle = getRandomColor()),
-            (exitBricks[i].color = ctx.fillStyle),
-            (exitBricks[j].color = ctx.fillStyle));
-      }
-    }
-    exitBricks[i].color
-          ? (ctx.fillStyle = exitBricks[i].color)
-          : ((ctx.fillStyle = getRandomColor()),
-            (exitBricks[i].color = ctx.fillStyle))
-    ctx.fillRect(
-      exitBricks[i].left,
-      exitBricks[i].top,
-      enterBricks[i].width,
-      enterBricks[i].height
-    );
-    ctx.fillStyle = "white";
-    ctx.fillRect(
-      exitParams.exitBricks[i].left + enterBricks[i].width * 0.5 - 15,
-      exitParams.exitBricks[i].top + enterBricks[i].height * 0.5 - 15,
-      30,
-      30
-    );
-    ctx.font = "25px Arial";
-    ctx.fillStyle = "black";
-    if (i < 10) {
-      ctx.fillText(
-        i,
-        exitParams.exitBricks[i].left + enterBricks[i].width * 0.5 - 7,
-        exitParams.exitBricks[i].top + enterBricks[i].height * 0.5 + 8
+function drawBricks(ctx, canvas) {
+  for (const brick of canvas.bricks) {
+    brick.color
+      ? (ctx.fillStyle = brick.color)
+      : ((ctx.fillStyle = getRandomColor()), (brick.color = ctx.fillStyle));
+    ctx.fillRect(brick.left, brick.top, brick.width, brick.height);
+
+    if (brick.initialOrder >= 0) {
+      ctx.fillStyle = "white";
+      ctx.fillRect(
+        brick.left + brick.width / 2 - 12.5,
+        brick.top + brick.height / 2 - 12.5,
+        25,
+        25
       );
-    } else {
-      ctx.fillText(
-        i,
-        exitParams.exitBricks[i].left + enterBricks[i].width * 0.5 - 15,
-        exitParams.exitBricks[i].top + enterBricks[i].height * 0.5 + 8
-      );
+      ctx.save()
+      ctx.translate(brick.left + brick.width / 2 + 10, brick.top + brick.height / 2 - 7)
+      ctx.rotate(Math.PI);
+      ctx.fillStyle = "black";
+      ctx.font = "20px Arial";
+      ctx.fillText(brick.initialOrder, 0, 0);
+      ctx.restore()
     }
+
+    // else {
+    //   brick.color
+    //     ? (ctx.fillStyle = brick.color)
+    //     : ((ctx.fillStyle = getRandomColor()), (brick.color = ctx.fillStyle));
+    //   ctx.fillRect(brick.left, brick.top, brick.width, brick.height);
+    // }
   }
 }
 
@@ -115,7 +197,6 @@ const getElems = () => {
   const ctx = c.getContext("2d");
 
   const fullnessPlace = document.querySelector("#fullnessPlace");
-
   return { app, ctx, fullnessPlace };
 };
 
@@ -123,28 +204,25 @@ const getElems = () => {
 const setCanvasProperies = (ctx, app) => {
   ctx.canvas.width = app.clientWidth;
   ctx.canvas.height = app.clientHeight * 0.9;
-
   //color
   ctx.fillStyle = "beige";
   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 };
 
-const watchCanvasChanges = () => {
-  const { app, ctx, fullnessPlace } = getElems();
-  window.addEventListener("resize", () => {
-    setCanvasProperies(ctx, app);
-    drawBricks(exitParams.exitBricks, enterBricks, ctx);
-  });
-};
-
-const init = () => {
+function init() {
   const { app, ctx, fullnessPlace } = getElems();
   setCanvasProperies(ctx, app);
-  searchPlace(enterBricks, ctx);
-  drawBricks(exitParams.exitBricks, enterBricks, ctx);
-  fullnessPlace.textContent = exitParams.fullness * 100 + "%";
-  watchCanvasChanges();
-  console.log(countFullness(enterBricks, exitParams.exitBricks, ctx));
-};
+  let bestBrickLayout = findBestBrickLayout(
+    enterBricks,
+    ctx.canvas.width,
+    ctx.canvas.height
+  );
+
+  fullnessPlace.textContent =
+    (bestBrickLayout.countFullness() * 100).toFixed(2) + "%";
+  drawBricks(ctx, bestBrickLayout);
+}
 
 init();
+
+window.addEventListener("resize", init);
