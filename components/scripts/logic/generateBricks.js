297c5c765ac4e6c803f779e096455a1c3ba26a5c
TheAlexGET <sucevanalex06@gmail.com>
2024-04-06 16:09:35 +0200

    better generation

--- components/scripts/logic/generateBricks.js
+++ components/scripts/logic/generateBricks.js
@@ -1,7 +1,12 @@
+import { getElems } from "../UI/setCanvas.js"
+
 export const generateBricks = () => {
+  const {canvas} = getElems()
+  const maxWidth = canvas.width * 0.2
+  const maxHeight = canvas.height * 0.2
   const bricks = []
-  for (let i = 0; i < 3; i++) {
-    bricks.push({width: Number((Math.random()*370).toFixed())+20, height: Number((Math.random()*370).toFixed())+20})
+  for (let i = 0; i < 10; i++) {
+    bricks.push({width: Number((Math.random()*maxWidth).toFixed())+25, height: Number((Math.random()*maxHeight).toFixed())+25})
   }
   return bricks
 }
